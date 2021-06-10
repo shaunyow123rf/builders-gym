947c15a255b385ce59d779ea5bb44a6f2c1e7074
Shaun Yow <shaun.yow@123rf.com>
2021-06-10 14:17:27 +0800

    installed service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,3 +70,27 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener('push', (event) => {
+  var data = {};
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  if (!(self.Notification && self.Notification.permission === 'granted')) 
+      return;
+
+  if (event.data) 
+      data = event.data.json();
+
+  // Customize the UI for the message box 
+  var title = data.title || "Web Push Notification";
+  var message = data.message || "New Push Notification Received";
+  var icon = "images/notification-icon.png";
+  var badge = 'images/notification-badge.png';
+  var options = {
+      body: message,
+      icon: icon,
+      badge: badge
+  };
+
+  event.waitUntil(self.registration.showNotification(title,options));
+});
